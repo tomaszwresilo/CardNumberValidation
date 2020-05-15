e1eaff52fafe92d4c33ebef7bd6bf9effd4bc0b2
Tomasz <twresilo@gmail.com>
2020-05-15 16:00:34 +0200

    es6 modules v1

--- index.js
+++ index.js
@@ -78,21 +78,33 @@ document.getElementById("check").onclick = function verifySupplier() {
 
         if ([totalCountOddNumbers + totalCountEvenNumbers] % 10 == 0) {
 
-
             // Funckcja która sprawdza warunki i zwraca returnem nazwy.
+            function checkLength(clearCardNumber){
 
             if (clearCardNumber.length == 16 && clearCardNumber[0] == 5 && clearCardNumber[1] == 1 || clearCardNumber[0] == 5 && clearCardNumber[1] == 2 || clearCardNumber[0] == 5 && clearCardNumber[1] == 3 || clearCardNumber[0] == 5 && clearCardNumber[1] == 4 || clearCardNumber[0] == 5 && clearCardNumber[1] == 5 || clearCardNumber[0] == 2 && clearCardNumber[1] == 2) {
 
-                document.getElementById("resolut").innerHTML = "MasterCard"
+               return "MasterCard";
+
+
             } else if (clearCardNumber.length == 13 || clearCardNumber.length == 16 && clearCardNumber[0] == 4) {
 
-                document.getElementById("resolut").innerHTML = "Visa";
+                return "Visa";
+
+
             } else if (clearCardNumber.length == 15 && clearCardNumber[0] == 3 && clearCardNumber[1] == 4 || clearCardNumber[0] == 3 && clearCardNumber[1] == 7) {
 
-                document.getElementById("resolut").innerHTML = "American Express";
+                return "American Express";
+
+
             } else {
-                document.getElementById("resolut").innerHTML = "Only Visa, Mastercard and American Express credit card checking is working!";
+
+                return "Only Visa, Mastercard and American Express credit card checking is working!";
+
             }
+
+        }
+
+        document.getElementById("resolut").innerHTML = checkLength(clearCardNumber);
 
         }
 
